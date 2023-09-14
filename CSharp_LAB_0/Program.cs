1a3b9cd95135e4ac7c051a1a5af238b37a5b6979
march <marchenkopavel2006@gmail.com>
2023-09-14 20:49:08 +0200

    Random Number Generator

--- CSharp_LAB_0/Program.cs
+++ CSharp_LAB_0/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace CSharp_LAB_0
+﻿using System;
+
+namespace CSharp_LAB_0
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            // Number Generator [-50, 50]
+            double number1, number2;
             
+            Random random = new(); 
+            number1 = random.NextDouble() * 100 - 50;
+            number2 = random.NextDouble() * 100 - 50;
+            
+            Console.WriteLine($"number1 - {number1} and number2 - {number2}");
         }
     }
 } 
