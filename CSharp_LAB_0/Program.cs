54461d34b2ee636f81d81c641736247e49c7b4a1
march <marchenkopavel2006@gmail.com>
2023-09-14 21:07:27 +0200

    Added function SquareOfDifference

--- CSharp_LAB_0/Program.cs
+++ CSharp_LAB_0/Program.cs
@@ -13,7 +13,20 @@ namespace CSharp_LAB_0
             number1 = random.NextDouble() * 100 - 50;
             number2 = random.NextDouble() * 100 - 50;
             
-            Console.WriteLine($"number1 - {number1} and number2 - {number2}");
+            double result = SquareOfDifference(number1, number2);
+            Console.WriteLine($"Difference square {number1} and {number2} = {result}");
+        }
+        /// <summary>
+        /// Calculate the square of the difference two numbers.
+        /// </summary>
+        /// <param name="num1"> first number.</param>
+        /// <param name="num2"> second number.</param>
+        /// <returns>Number difference square.</returns>
+        static double SquareOfDifference(double num1, double num2)
+        {
+            double difference = num1 - num2;
+            double square = difference * difference;
+            return square;
         }
     }
 } 
